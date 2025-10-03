0c250f53cad0b7fecb9a9840bfaed4badc6e9f76
XxXNe0XxX <jm82467913@gmail.com>
2025-10-03 14:29:43 -0500

    fixed style

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -39,7 +39,7 @@ export default function RootLayout() {
   return (
     <div className="relative min-h-screen   ">
       {/* Sidebar/Drawer Navbar is positioned fixed; reserve space on md+ */}
-      <div className="absolute bg-background bg-center bg-[url('octopus.webp')] bg-blend-overlay bg-cover  bg-no-repeat inset-0  bg-fixed blur-xs z-0"></div>
+      {/* <div className="absolute bg-background bg-center bg-[url('octopus.webp')] bg-blend-overlay bg-cover  bg-no-repeat inset-0  bg-fixed blur-xs z-0"></div> */}
       <Navbar items={isDashboard ? dashboardNav : siteNav} className="z-20" />
       <div className="md:pl-64 z-20  ">
         {/* Mobile top-bar height spacer */}
